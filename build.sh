aa2b0032bf1f1f223943c7c6de824d327735966e
VML Developer <dev@vml.com>
2026-06-06 01:03:52 +0200

    fix: use curl instead of wget in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Installing Flutter..."
 cd /tmp
-wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.0-stable.tar.xz -O flutter.tar.xz
+curl -L https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.0-stable.tar.xz -o flutter.tar.xz
 tar xf flutter.tar.xz
 export PATH="/tmp/flutter/bin:$PATH"
 
